e0e612ef0eef284afd56836c231b635db5a1ef94
Varsha <varsha.ravi@metayb.ai>
2025-09-11 10:47:43 +0530

    Dashboard Page is created

---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "./LayoutPage";
+import Dashboard from "../components/Dashboard";
+
+export default function DashboardPage(){
+    return(
+        <Layout>
+            <Dashboard/>
+        </Layout>
+    )
+}
